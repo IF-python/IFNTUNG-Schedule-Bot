c490cffa229ce3e34aa052907bc1f22ee33de571
P-alban <alban@national.shitposting.agency>
2018-10-15 16:13:00 +0300

    collect xlsx to json format

--- utils.py
+++ utils.py
@@ -11,3 +11,4 @@ groups = [group(i[1], i[3]) for i in sheet.values if isinstance(i[0], int)]
 json.dump(obj={item.code: item.name for item in groups},
           fp=open('groups.json', 'w', encoding='utf8'),
           indent=4, ensure_ascii=False)
+
