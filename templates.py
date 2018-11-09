53e3a9dd01a61c98f756ad9967c1df5c1435a4d9
P-alban <alban@national.shitposting.agency>
2018-11-09 19:04:42 +0200

    added more info

--- templates.py
+++ templates.py
@@ -5,6 +5,9 @@ Users: {}\nSource code: [click](https://github.com/P-Alban/IFNTUNG-Schedule-Bot)
 _Ви можете відправляти 25 запитів розкладу на добу але не частіше ніж раз в 2 секунди._\n
 *У вас залишилось {}/25 запитів на сьогодні.*
 `will be updated at 00:00 (UTC)`
+
+/get - Показати мою групу
+/set - Змінити групу
 '''
 set_group_message = 'Ви обрали: {} ({})'
 not_found = 'Розклад не знайдений.'
