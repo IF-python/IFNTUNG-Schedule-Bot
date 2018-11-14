bf9f44b2f7ea3708a8c1d80dd128925111601d81
P-alban <alban@national.shitposting.agency>
2018-11-14 15:07:42 +0200

    little fix

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ CACHE_TIME = 3600 * 2  # 2 hours
 TIME_ZONE = pytz.timezone('Europe/Kiev')
 default_encoding = 'windows-1251'
 url = 'http://194.44.112.6/cgi-bin/timetable.cgi?n=700'
-timestamp_length = 11
+timestamp_length = 12
 days = {'Сьогодні': 0, 'Завтра': 1}
 requests_limit_per_day = 25
 throttle_time = 2
