3fe110d5c1d94595b549c3c30fc22a6f88e0d4a0
Pavel Durmanov <alban@national.shitposting.agency>
2019-01-23 16:30:27 +0200

    celery fix

--- notifications.py
+++ notifications.py
@@ -26,11 +26,10 @@ def notify(group, user_id, flag):
     bot.send_message(user_id, text=get_schedule('Сьогодні', group, bot, user_id, flag))
 
 
-@app.task
+@app.task(ignore_result=True)
 def main():
     current_time = datetime.datetime.now(TIME_ZONE).time().replace(second=0, microsecond=0)
     target_users = Student.at_time(current_time)
     for user in target_users:
         notify(user.group.group_code, user.student_id, user.extend)
         sleep(0.05)
-    return current_time, target_users
