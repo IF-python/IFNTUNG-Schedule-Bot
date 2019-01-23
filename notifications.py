81b1ad386e1e3728925a7def7d72eb502cc35625
Pavel Durmanov <alban@national.shitposting.agency>
2019-01-23 14:14:52 +0200

    fix celery task

--- notifications.py
+++ notifications.py
@@ -1,9 +1,10 @@
-import datetime
+♣import datetime
 import os
 from time import sleep
 
-from celery import Celery, Task
+from celery import Celery
 from celery.schedules import crontab
+from peewee import Proxy
 
 from config import TIME_ZONE
 from main import bot
@@ -11,30 +12,21 @@ from models import Student, db
 from utils import get_schedule
 
 app = Celery('notifications', broker=os.environ.get('REDIS_URL'))
-
 app.conf.beat_schedule = {
     'notify_every_week_day': {
         'task': 'notifications.main',
         'schedule': crontab()
     },
 }
-
-
-class DatabaseTask(Task):
-    _db = None
-
-    @property
-    def db(self):
-        if self._db is None:
-            self._db = db.connect()
-        return self._db
+database_proxy = Proxy()
+database_proxy.initialize(db)
 
 
 def notify(group, user_id, flag):
     bot.send_message(user_id, text=get_schedule('Сьогодні', group, bot, user_id, flag))
 
 
-@app.task(base=DatabaseTask)
+@app.task
 def main():
     current_time = TIME_ZONE.localize(datetime.datetime.now()).time().replace(second=0, microsecond=0)
     target_users = Student.at_time(current_time)
